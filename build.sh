5fc971749f827ec6415f42ea5bc43994966ccf6c
davidpodhola <david.podhola@naseukoly.cz>
2016-11-05 09:35:15 +0100

    trying to fix https://github.com/fsprojects/Paket/issues/1809

--- build.sh
+++ build.sh
@@ -21,9 +21,13 @@ then
 else
   # use mono
   mono .paket/paket.bootstrapper.exe
+    
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
+    cd .paket
+    wget https://github.com/fsprojects/Paket/releases/download/3.26.3/paket.exe
+    wget https://github.com/fsprojects/Paket/releases/download/3.26.3/paket.targets  
+    cd ..
   fi
 
   mono .paket/paket.exe restore
